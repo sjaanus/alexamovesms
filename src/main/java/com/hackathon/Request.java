da0631bae2eacac4f6127b613bfda76ecf0e1637
Jaanus Sellin <sellinjaanus@gmail.com>
2017-09-30 13:41:54 +0300

    Update pojo

--- src/main/java/com/hackathon/Request.java
+++ src/main/java/com/hackathon/Request.java
@@ -7,9 +7,27 @@ import lombok.Setter;
 /**
  * Created by Jaanus on 30.09.2017.
  */
-@Getter
-@Setter
+//@Getter
+//@Setter
 public class Request {
 	@JsonProperty("message")
 	private Message message;
+	private String test;
+
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+
+	public Message getMessage() {
+		return message;
+	}
+
+	public void setMessage(Message message) {
+		this.message = message;
+	}
 }
